766621b0af9eeae384f79b9ce8f588156c111b0a
ThuPham02 <s3878246@rmit.edu.vn>
2021-12-11 15:40:33 +0700

    Add first draft content html, js and style folders

---
+++ js/footer.js
@@ -0,0 +1,11 @@
+$(function () {
+    $("#footer").load("footer.html");
+    $("#footer").css({
+        "background-color": "#f7fbfc",
+        "text-align": "center",
+        "padding": "10px",
+        "height": "100px",
+        "bottom": "0",
+        "width": "100%",
+    })
+});
---
+++ js/scroll.js
@@ -0,0 +1,45 @@
+// Project Ideals Section
+ScrollReveal({
+    reset:false,
+    duration: 1500,
+})
+
+ScrollReveal().reveal('.overview', { 
+    delay: 300,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.motivation', {
+    delay: 500,
+    origin: 'bottom',
+})
+ScrollReveal().reveal('.description', { 
+    delay: 700,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.tool-tech', { 
+    delay: 900,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.skill', { 
+    delay: 900,
+    origin: 'bottom',
+});
+
+ScrollReveal().reveal('.outcome', { 
+    delay: 900,
+    origin: 'bottom',
+});
+
+// Team Profile Sections
+ScrollReveal().reveal('#team', { 
+    delay: 500,
+    origin: 'bottom',
+});
+ScrollReveal().reveal('#moreInfo', { 
+    delay: 700,
+    origin: 'bottom',
+});
+
